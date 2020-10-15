e21fa79884505542f5dcd2a8288b748ad62a7611
Eligh100 <elighjohn11@outlook.com>
2020-10-15 16:28:03 +0100

    Finished game implementation

--- BullCowGame-starter-kit/Source/BullCowGame/BullCowCartridge.cpp
+++ BullCowGame-starter-kit/Source/BullCowGame/BullCowCartridge.cpp
@@ -1,16 +1,23 @@
 // Fill out your copyright notice in the Description page of Project Settings.
 #include "BullCowCartridge.h"
+#include "Misc/FileHelper.h"
+#include "Misc/Paths.h"
 
 void UBullCowCartridge::BeginPlay() // When the game starts
 {
     Super::BeginPlay();
 
-    PrintLine(TEXT("The hidden word is: %s"), *HiddenWord); // Debug line
+    // Loading in the hidden words
+    const FString WordListPath = FPaths::ProjectContentDir() / TEXT("WordLists/HiddenWordList.txt");
+    FFileHelper::LoadFileToStringArrayWithPredicate(Isograms, *WordListPath, [](const FString& CurrentWord)
+    {
+       return CurrentWord.Len() >= 4 && CurrentWord.Len() <= 8 && IsIsogram(CurrentWord.ToLower());
+    }); 
 
     SetupGame();
 }
 
-void UBullCowCartridge::OnInput(const FString& Input) // When the player hits enter
+void UBullCowCartridge::OnInput(const FString& PlayerInput) // When the player hits enter
 {
     if (bGameOver)
     {
@@ -19,7 +26,7 @@ void UBullCowCartridge::OnInput(const FString& Input) // When the player hits en
     }
     else  // Checking player's guess
     {
-        ProcessGuess(Input);
+        ProcessGuess(PlayerInput);
     }
 }
 
@@ -28,11 +35,13 @@ void UBullCowCartridge::SetupGame()
     // Welcoming the player
     PrintLine(TEXT("Welcome to the Bull Cow Game!"));
 
-    HiddenWord = TEXT("cake");
+    HiddenWord = Isograms[FMath::RandRange(0, Isograms.Num()-1)];
     Lives = HiddenWord.Len();
     bGameOver = false;
 
     PrintLine(TEXT("Please guess the %i-letter word\nYou have %i lives\nPress 'Enter' to continue..."), HiddenWord.Len(), Lives);
+    PrintLine(TEXT("The hidden word is: %s"), *HiddenWord); // Debug line
+
 }
 
 void UBullCowCartridge::EndGame()
@@ -41,7 +50,7 @@ void UBullCowCartridge::EndGame()
     PrintLine(TEXT("Press enter to play again..."));
 }
 
-void UBullCowCartridge::ProcessGuess(FString Guess)
+void UBullCowCartridge::ProcessGuess(const FString& Guess)
 {
     // Correct guess
     if (HiddenWord == Guess) 
@@ -54,7 +63,7 @@ void UBullCowCartridge::ProcessGuess(FString Guess)
     // Wrong length for guess
     if (Guess.Len() != HiddenWord.Len())
     {
-        PrintLine(TEXT("Your guess is the wrong number of characters\nThe hidden word is %i-letters long"), HiddenWord.Len());
+        PrintLine(TEXT("Your guess is the wrong length\nThe hidden word is %i-letters long"), HiddenWord.Len());
         return;
     } 
 
@@ -65,7 +74,7 @@ void UBullCowCartridge::ProcessGuess(FString Guess)
         return;
     }
 
-    PrintLine(TEXT("You have lost a life\nYou have %i lives remaining"), Lives);
+    PrintLine(TEXT("You have lost a life"));
 
     if (--Lives <= 0)
     {
@@ -74,9 +83,16 @@ void UBullCowCartridge::ProcessGuess(FString Guess)
         EndGame();
         return; 
     }
+
+    // Show the player Bulls and Cows
+    FBullCowCount Score = GetBullCows(Guess);
+
+    PrintLine(TEXT("You have %i Bulls and %i Cows"), Score.Bulls, Score.Cows);
+
+    PrintLine(TEXT("Guess again, you have %i lives remaining"), Lives);
 }
 
-bool UBullCowCartridge::IsIsogram(FString Word) const
+bool UBullCowCartridge::IsIsogram(const FString& Word)
 {
     for (int32 i = 0; i < Word.Len(); i++)
     {
@@ -90,4 +106,29 @@ bool UBullCowCartridge::IsIsogram(FString Word) const
     }
     
     return true;
+}
+
+FBullCowCount UBullCowCartridge::GetBullCows(const FString& Guess) const
+{
+    FBullCowCount Count;
+
+    for (int32 GuessIndex = 0; GuessIndex < Guess.Len(); GuessIndex++)
+    {
+        if (Guess[GuessIndex] == HiddenWord[GuessIndex])
+        {
+            Count.Bulls++;
+            continue;
+        }
+
+        for (int32 HiddenWordIndex = 0; HiddenWordIndex < HiddenWord.Len(); HiddenWordIndex++)
+        {
+            if (Guess[GuessIndex] == HiddenWord[HiddenWordIndex])
+            {
+                Count.Cows++;
+                break;
+            }
+        }
+    }
+
+    return Count;
 }
